ce1dc2e9639d23e292ebbfd135afb1e1241d85f0
theo <theo.lahmar1@gmail.com>
2023-12-29 01:22:38 +0100

    included header files

--- tcp_client.c
+++ tcp_client.c
@@ -1,3 +1,14 @@
 //
 // Created by yyy on 29/12/2023.
 //
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
